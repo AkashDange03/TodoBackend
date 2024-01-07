c6cc9627840ddfc432c490383cfffb2bcbb4593c
Akash_03 <akash.dange0307@gmail.com>
2024-01-07 14:58:43 +0530

    chagne main property in package.json

--- src/data/database.js
+++ src/data/database.js
@@ -4,6 +4,6 @@ export const connect = () => {
     mongoose.connect(process.env.MONGO_URI,{
         dbName:"BackendTodo"
     })
-    .then(() => console.log("database connected succesfully"))
+    .then((c) => console.log(`database connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 }
